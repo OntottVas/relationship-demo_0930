88194c83f2da6604de6aabbb0918b380b8192276
Geree <smithzero94@gmail.com>
2023-09-30 14:21:26 +0200

    application created, database set

---
+++ src/main/java/hu/progmatic/relationshipdemo_0930/RelationshipDemo0930Application.java
@@ -0,0 +1,13 @@
+package hu.progmatic.relationshipdemo_0930;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationshipDemo0930Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelationshipDemo0930Application.class, args);
+    }
+
+}
---
+++ src/test/java/hu/progmatic/relationshipdemo_0930/RelationshipDemo0930ApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.progmatic.relationshipdemo_0930;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationshipDemo0930ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
